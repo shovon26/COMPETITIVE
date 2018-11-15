353e91ffa4f58f7dd844aefb22c342260c7fecb4
shovon26 <36441192+shovon26@users.noreply.github.com>
2018-11-15 22:34:32 +0600

    Add files via upload

---
+++ Template.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#include<cstring>
+#define ll long long
+#define pi acos(-1.0)
+#define pb push_back
+#define clr(a) memset(a,0,sizeof a)
+#define reset(a) memset(a,-1,sizeof a)
+#define pii pair<int,int>
+using namespace std;
+const int sz=15;
+int fx[]= {1,0,-1,0};
+int fy[]= {0,1,0,-1};
+
+int main()
+{
+
+   int i,j,k,l,m,tc=0,t;
+
+    return 0;
+
+}
+
