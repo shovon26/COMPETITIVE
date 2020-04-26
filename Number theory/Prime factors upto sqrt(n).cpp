fb57b878cd2efda5d11d03c7107b0897b909f874
Md.Shahriyar Chowdhury Shovon <36441192+shovon26@users.noreply.github.com>
2020-04-26 16:22:38 +0600

    Create Prime factors upto sqrt(n).cpp

---
+++ Number theory/Prime factors upto sqrt(n).cpp
@@ -0,0 +1,13 @@
+vector<int> PrimeFactors(int n)
+{
+	vector<int>factor;
+	for (int i = 2; i * i <= n; i++) {
+		while (n % i == 0) {
+			factor.push_back(i);
+			n /= i;
+		}
+	}
+	if (n >= 2)
+		factor.push_back(n);
+	return factor;
+}
