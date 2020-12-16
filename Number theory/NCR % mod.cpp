ebdd62f64dbad237b4fccd19b5b92ac863317077
Md.Shahriyar Chowdhury Shovon <36441192+shovon26@users.noreply.github.com>
2020-12-16 21:00:31 +0600

    Update NCR % mod.cpp

--- Number theory/NCR % mod.cpp
+++ Number theory/NCR % mod.cpp
@@ -1,24 +1,12 @@
-#include<bits/stdc++.h>
-#define ss second
-#define ff first
-#define Mem(x,val) memset(x,val,sizeof x)
-#define pb push_back
-#define all(v) v.begin(),v.end()
-#define unik(v)  v.erase(unique(v.begin(),v.end()),v.end())
-#define cerr(x) cerr << __LINE__ << " th Line : " << #x << " = " << x << "\n"
-#define debug(a,b) cerr << #a<<" = "<<a<<"   |   "<<#b<<" = "<<b<<'\n'
-#define endl '\n'
-#define SZ(a) ((int)a.size())
-using namespace std;
-using ll = long long;
-typedef pair<int, int> pi;
-typedef pair<ll, ll> pll;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef vector<pi> vpi;
-typedef vector<pll> vpll;
-const ll mod = 1000003;
-const int nax = 1000000+5;
+const ll mod = 1e9+7;
+const int nax = 2e5+5;
+ll fact[nax+10];
+void factorize(){
+	fact[1] = 1;
+	for(int i=2; i<=nax; i++){
+		fact[i] = (i*fact[i-1]*1LL)%mod;
+	}
+}
 ll bigmod(ll a, ll b, ll m)
 {
     if (b == 0 )return 1;
@@ -37,37 +25,11 @@ ll modInverse(ll a, ll m)
 {
     return bigmod(a, m-2, m);
 }
-
-ll fact[nax+10];
-void factorize(){
-	fact[1] = 1;
-	for(int i=2; i<=1000000; i++){
-		fact[i] = (i*fact[i-1]*1LL)%mod;
-	}
-}
-
-int main()
-{
-   ios_base::sync_with_stdio(0);
-   cin.tie(0); cout.tie(0);
-   
-   factorize();
-   int _;
-   scanf("%d", &_);
-   int tc = 0;
-   while(_--){
-	   int n, k;
-	   scanf("%d %d", &n, &k);
-	   if(n == k || k == 0){
-		   printf("Case %d: 1\n", ++tc);
-		   continue;
-	   }
-	   ll ans = 0;
-	   ans = ((fact[k]%mod) * (fact[n-k]%mod)) % mod;
-	   ans = modInverse(ans, mod);
-	   ans = ((fact[n]%mod) * (ans%mod)) % mod;
-	   printf("Case %d: %lld\n", ++tc, ans);
-   }
-   
-   return 0;
+ll NCR(ll n, ll r){
+	if(n == r || r == 0) return 1;
+	if(n < r || r < 0) return 0;
+	ll ans = ((fact[r]%mod) * (fact[n-r]%mod)) % mod;
+	ans = modInverse(ans, mod);
+	ans = ((ans%mod) * (fact[n]%mod)) % mod;
+	return ans;
 }
